c9ed3ccd806535c36009821e99a06e3725502603
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-06-09 07:40:10 +0545

    js Projects

---
+++ COLORFLIPPER/index.js
@@ -0,0 +1,6 @@
+const body = document.getElementsByTagName("body")[0];
+// body.style.backgroundColor="red";
+
+document.addEventListener("click", function red() { body.style.backgroundColor = "red" });
+
+// console.log(body);

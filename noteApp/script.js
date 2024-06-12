51531f04e3b2885240518763dfabce4d5e930b9c
Krishna Kumar Mandal <mandalkrishna739@gmail.com>
2024-06-12 16:23:35 +0545

    arko hawa

---
+++ noteApp/script.js
@@ -0,0 +1,51 @@
+const addBtn = document.querySelector("#addBtn")
+const main = document.querySelector("#main")
+
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea")
+    const data = []
+    notes.forEach(
+        (note) => {
+            data.push(note.value)
+        }
+    )
+    console.log(data);
+    localStorage.setItem("notes", JSON.stringify(data))
+}
+
+(
+    function() {
+        const lsNotes = JSON.parse(localStorage.getItem("notes"))
+        console.log(lsNotes)
+    }
+)
+
+addBtn.addEventListener("click", function () {
+    addNote()
+})
+
+const addNote = () => {
+    const note = document.createElement("div")
+    note.classList.add("note")
+    note.innerHTML = `
+        <div class="tool">
+            <i class="save fa-solid fa-floppy-disk"></i>
+            <i class="trash fa-solid fa-trash"></i>
+        </div>
+        <textarea></textarea>
+    `;
+
+    note.querySelector(".trash").addEventListener("click",
+        function () {
+            note.remove()
+            saveNotes()
+        })
+
+    note.querySelector(".save").addEventListener("click",
+        function () {
+            saveNotes()
+        })
+
+    main.appendChild(note);
+    saveNotes()
+}
